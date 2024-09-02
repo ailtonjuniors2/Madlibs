168f0868747e2034be7bb494c05c3724fe4e1bed
Junior <ajunior220905@gmail.com>
2024-09-02 16:54:32 -0300

    first commit

---
+++ madlibs.py
@@ -0,0 +1,30 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+animal1 = input("Animal: ")
+animal2 = input("Animal: ")
+pluralfood = input("Plural food: ")
+game = input("Game: ")
+pluralanimal = input("Plural animal: ")
+verb_ending_with_ing = input("Verb ending with 'ing': ")
+verb_ending_with_ing2 = input("Verb ending with 'ing': ")
+pluralnoun1 = input("Plural noun: ")
+verb_ending_with_ing3 = input("Verb ending with 'ing': ")
+noun1 = input("Noun: ")
+animal3 = input("Animal: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+number = input("Number: ")
+pluralnoun2 = input("Plural noun: ")
+noun4 = input("Noun: ")
+
+madlib = f"""
+Story: 'A Day at the Zoo'
+It was a {adj1}, {adj2} day at the zoo. My best friend and I decided to visit the zoo to see the {animal1} and the {animal2}. We bought some {pluralfood} to feed the animals. After that, we played a quick game of {game}.
+We were having so much fun that we didn't notice the {pluralanimal} that had escaped from their enclosure! They were {verb_ending_with_ing} all over the place. We tried {verb_ending_with_ing2} them with our {pluralnoun1}, but it was no use. They were too busy {verb_ending_with_ing3} around the {noun1}.
+Suddenly, a {animal3} appeared! It started {verb_ending_with_ing} towards us, and we ran as fast as we could. We hid behind a {noun2} and hoped it wouldn't find us. Luckily, a zookeeper arrived with a {noun3} and led the animal back to its pen.
+Feeling {adj3} and {adj4}, we decided to leave the zoo and head home. As we walked out, we realized we had spent {number} hours there! We had so much fun, and we couldn't wait to tell our friends all about our adventure with the {pluralnoun2} and the {noun4}!
+"""
+
+print(madlib)
